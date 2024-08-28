e3f19ec41fca0c46fd66528157037e5fe22119c8
iannlucs <iangallego82@gmail.com>
2024-08-27 22:00:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function renaper(ctx) {
         return;
     }
     ctx.reply("Buscando...");
-    axios.get(`https://ricardoaplicaciones-github-io.onrender.com/api/federador/${dni}/${sexo}`)
+    axios.get(`https://ricardoaplicaciones-qu4k.onrender.com/api/federador/${dni}/${sexo}`)
         .then(response => {
             const data = response.data;
             if ("data" in data && "sisa" in data.data) {
